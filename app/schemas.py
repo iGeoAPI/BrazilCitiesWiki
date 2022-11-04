a8301d660e8f6bbad9907ccdc5ba4ca2450fc35c
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2022-11-04 01:47:48 -0300

    nothingburger
    changed the position of the attributes of the object-schema city

--- app/schemas.py
+++ app/schemas.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel, Field
 
 
 class City(BaseModel):
-    ibge_code: int = Field(default=None, description='Brazilian Institute of Geography and Statistics')
-    name: str = Field(default=None, description='Name of the city')
-    federative_unit: str = Field(default=None, description='Federative Unit/State')
-    motto: str = Field(default=None, description='City motto')
-    zip_range: str = Field(default=None, description='Zip range')
-    anthem: str = Field(default=None, description='The city anthem')
     flag: str = Field(default=None, description='City flag')
+    motto: str = Field(default=None, description='City motto')
     blazon: str = Field(default=None, description='City blazon')
+    zip_range: str = Field(default=None, description='Zip range')
+    name: str = Field(default=None, description='Name of the city')
+    anthem: str = Field(default=None, description='The city anthem')
     total_area: int = Field(default=None, description='Total area km²')
+    federative_unit: str = Field(default=None, description='Federative Unit/State')
+    ibge_code: int = Field(default=None, description='Brazilian Institute of Geography and Statistics')
